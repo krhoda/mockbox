f05d87c9cede3a620bb79ab1703aacc1b3c693e7
K Rhoda <kelseydrhoda@gmail.com>
2020-09-24 22:03:37 -0600

    Delete and upload work

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,19 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
-use std::path::{Path, PathBuf};
-use rocket::http::Method;
+use rocket::http::{Method, Status};
 use rocket::response::NamedFile;
+use rocket_contrib::json::{Json, JsonValue};
 use rocket_cors::{AllowedHeaders, AllowedOrigins};
-use rocket_contrib::json::{JsonValue};
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 const FILE_DIR: &str = "./assets";
 
@@ -16,16 +21,17 @@ const FILE_DIR: &str = "./assets";
 fn make_cors() -> Result<rocket_cors::Cors, rocket_cors::Error> {
     let allowed_origins = AllowedOrigins::all();
 
-    rocket_cors::CorsOptions{
+    rocket_cors::CorsOptions {
         allowed_origins,
-        allowed_methods: vec![Method::Get, Method::Post, Method::Options]
-        .into_iter()
-        .map(From::from)
-        .collect(),
+        allowed_methods: vec![Method::Delete, Method::Get, Method::Post, Method::Options]
+            .into_iter()
+            .map(From::from)
+            .collect(),
         allowed_headers: AllowedHeaders::all(),
         allow_credentials: true,
         ..Default::default()
-    }.to_cors()
+    }
+    .to_cors()
 }
 
 fn file_to_file_rec(f: std::fs::DirEntry) -> Option<FileRec> {
@@ -33,20 +39,18 @@ fn file_to_file_rec(f: std::fs::DirEntry) -> Option<FileRec> {
         Ok(name) => {
             let name2 = name.clone();
             let maybe_ext = std::path::Path::new(&name2)
-            .extension()
-            .and_then(std::ffi::OsStr::to_str);
+                .extension()
+                .and_then(std::ffi::OsStr::to_str);
 
             match maybe_ext {
-                Some(ext) => {
-                    Some(FileRec{
-                        name: name,
-                        ext: String::from(ext),
-                    })
-                }
-                _ => None
+                Some(ext) => Some(FileRec {
+                    name: name,
+                    ext: String::from(ext),
+                }),
+                _ => None,
             }
-        },
-        _ => None
+        }
+        _ => None,
     }
 }
 
@@ -54,7 +58,13 @@ fn file_to_file_rec(f: std::fs::DirEntry) -> Option<FileRec> {
 #[derive(Serialize, Deserialize)]
 struct FileRec {
     name: String,
-    ext: String
+    ext: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct FileUp {
+    name: String,
+    body: String,
 }
 
 // ROUTES:
@@ -71,31 +81,55 @@ fn download_file(file: PathBuf) -> Option<NamedFile> {
 #[get("/files")]
 fn list_files() -> Result<JsonValue, std::io::Error> {
     let entries = std::fs::read_dir(FILE_DIR)?
-    .map(|res| {
-        res.map(|entry| file_to_file_rec(entry))
-    })
-    .filter(|maybe_rec| {
-        match maybe_rec {
+        .map(|res| res.map(|entry| file_to_file_rec(entry)))
+        .filter(|maybe_rec| match maybe_rec {
             Ok(x) => match x {
                 Some(_) => true,
-                _ => false
+                _ => false,
             },
-            _ => false
-        }
-    })
-    .collect::<Result<Vec<_>, std::io::Error>>()?;
+            _ => false,
+        })
+        .collect::<Result<Vec<_>, std::io::Error>>()?;
 
     Ok(json!(entries))
+}
+
+#[delete("/files/<file_name..>")]
+fn delete_file(file_name: PathBuf) -> Result<Status, std::io::Error> {
+    std::fs::remove_file(Path::new(FILE_DIR).join(file_name))?;
+    Ok(Status::Ok)
+}
+
+#[post("/files", format = "application/json", data = "<file_up>")]
+fn upload_file(file_up: Json<FileUp>) -> Result<Status, std::io::Error> {
+    let maybe_b = base64::decode(&file_up.0.body);
+
+    match maybe_b {
+        Ok(b) => {
+            let mut next_file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create_new(true)
+                .open(Path::new(FILE_DIR).join(file_up.0.name))?;
+
+            next_file.write_all(&b)?;
+
+            Ok(Status::Ok)
+        }
+        _ => {
+            Ok(Status::InternalServerError)
+        }
+    }
 }
 
 fn main() {
     match make_cors() {
         Ok(cors) => {
             rocket::ignite()
-            .mount("/", routes![hello, download_file, list_files])
-            .attach(cors)
-            .launch();
-        },
+                .mount("/", routes![hello, download_file, list_files, upload_file, delete_file])
+                .attach(cors)
+                .launch();
+        }
         Err(err) => {
             panic!("Failed in CORS creation, err: {}", err);
         }
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const serverBase = "http://localhost:8000/"
@@ -7,7 +6,32 @@ const makeURL = (suffix) => {
   return `${serverBase}${suffix}`;
 }
 
-const getFile = (targetFile, errHandler) => {
+const getFileList = (listHandler, errHandler) => {
+  let targetURL = makeURL("files");
+
+  fetch(targetURL)
+    .then((result) => {
+      if (!result.ok) {
+        return errHandler(`Received bad status code in init: ${result.status}`)
+      }
+      result
+        .json()
+        .then((json) => {return listHandler(json)})
+        .catch((err) => {
+          console.log("Failed in JSON parse:")
+          console.log(err);
+          errHandler(`${err}`);
+        })
+    })
+    .catch((err) => {
+      console.log("Failed in Fetch:")
+      console.log(err);
+      errHandler(`${err}`);
+    })
+}
+
+
+const downloadFile = (targetFile, errHandler) => {
   let targetPath = `files/${targetFile}`;
   let targetURL = makeURL(targetPath);
   fetch(targetURL)
@@ -32,33 +56,59 @@ const getFile = (targetFile, errHandler) => {
     })
 }
 
+const uploadFile = (opts, optsHandler, listHandler, errHandler) => {
+  if (!opts) {
+    return errHandler("No file selected");
+  }
+
+  let targetURL = makeURL("files");
+  fetch(targetURL, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(opts)
+  }).then((res) => {
+    if (!res.ok) {
+      console.error("Failed in upload Request:")
+      console.error(res);
+      return errHandler("Failed in upload request, does the file already exist?");
+    }
+
+    optsHandler(false);
+    document.querySelector('#suploader').value = null;
+
+    return getFileList(listHandler, errHandler);
+  })
+}
+
+const deleteFile = (targetFile, listHandler, errHandler) => {
+  let targetPath = `files/${targetFile}`;
+  let targetURL = makeURL(targetPath);
+
+  fetch(targetURL, {method: "DELETE"})
+    .then((res) => {
+      if (!res.ok) {
+        console.error("Failed in Delete Req:")
+        console.error(res);
+
+        errHandler("Failed in Delete Request, reloading list...");
+      }
+
+      getFileList(listHandler, errHandler);
+    })
+    .catch((err) => {
+      console.error("Failed in Delete Req:")
+      console.error(err);
+      errHandler(err);
+    })
+}
+
 function App() {
   let [errStatus, setErrStatus] = useState('');
   let [fileList, setFileList] = useState([]);
+  let [upFileOpts, setUpFileOpts] = useState(false);
 
   useEffect(() => {
-    let targetURL = makeURL("files");
-
-    fetch(targetURL)
-      .then((result) => {
-        if (!result.ok) {
-          return setErrStatus(`Received bad status code in init: ${result.status}`)
-        }
-        result
-        .json()
-        .then((json) => {return setFileList(json)})
-        .catch((err) => {
-          console.log("Failed in JSON parse:")
-          console.log(err);
-          setErrStatus(`${err}`);
-        })
-
-      })
-      .catch((err) => {
-        console.log("Failed in Fetch:")
-        console.log(err);
-        setErrStatus(`${err}`);
-      })
+    getFileList(setFileList, setErrStatus);
   }, []);
 
   let errHolder = '';
@@ -79,23 +129,92 @@ function App() {
         <div className="file-elm" key={`file-elm-${i}`}>
           <p>Name: {`${entry.name}`}</p>
           <p>Extension: {`${entry.ext}`}</p>
+
           <button
             onClick={(e) => {
               e.preventDefault();
-              return getFile(entry.name, setErrStatus);
+              return downloadFile(entry.name, setErrStatus);
             }}
           >
             Download
           </button>
+
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              return deleteFile(entry.name, setFileList, setErrStatus);
+            }}
+          >
+            Delete
+          </button>
+
         </div>
       );
       fileListHolder.push(elm);
     }
   }
 
+  let upBtn = '';
+  if (upFileOpts) {
+    upBtn = (
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          uploadFile(
+            upFileOpts, 
+            setUpFileOpts, 
+            setFileList, 
+            setErrStatus
+          );
+        }}
+      >
+        Upload!
+      </button>
+    );
+  }
+
   return (
     <div className="App">
       {errHolder} 
+      <div className="file-uploader">
+        <input 
+          type="file"
+          id="suploader"
+          onClick={(e) => {
+            e.target.value = null;
+            setUpFileOpts(false);
+          }}
+          onChange={(e) => {
+            let {value} = e.target;
+            let sep = "/";
+            if (value[0] !== "~" && value[0] !== "/") {
+              sep = "\\";
+            }
+            let valueList = value.split(sep);
+            let fname = valueList[valueList.length - 1];
+            let reader = new FileReader();
+
+            reader.onload = () => {
+              let fbody = reader.result.split(",");
+              let opts = {
+                name: fname,
+                body: fbody[fbody.length-1]
+              }
+              console.log(opts);
+              setUpFileOpts(opts);
+            }
+
+            reader.onerror = (err) => {
+              setErrStatus(err);
+            }
+
+            let file = document.querySelector('#suploader').files[0];
+            reader.readAsDataURL(file)
+          }}
+        ></input>
+        <br></br>
+        {upBtn}
+      </div>
       <div className="file-list-holder" >
         <h2>Uploaded Files:</h2>
         <div className="file-list">
